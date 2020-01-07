b6a55197722b5a48f4cf9e57eb745ee4373fb46a
NickM <nmikstas@alumni.scu.edu>
2020-01-06 17:38:45 -0700

    disabled key scrolling in the browser

--- assets/js/ntRenderTest.js
+++ assets/js/ntRenderTest.js
@@ -119,6 +119,17 @@ $( document ).ready(function()
     });
 });
 
+/************************************** Disable Key Scrolling **************************************/
+
+window.addEventListener("keydown", function(e)
+{
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1)
+    {
+        e.preventDefault();
+    }
+}, false);
+
 /************************************ Configuration Callback *************************************/
 
 configCallback = (input, type, value, status) =>
